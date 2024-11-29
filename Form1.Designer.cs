7f9c0a773885c0d0653778949c875b23c186f9f8
Gera <Gera@DESKTOP-GQSF1H9>
2024-11-29 14:52:27 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace vacation
+{
+    public partial class Form1 : Form
+    {
+        private Label lblEmployeeName;
+        private TextBox txtEmployeeName;
+        private Label lblEmployeePosition;
+        private TextBox txtEmployeePosition;
+        private Label lblStartDate;
+        private Label lblEndDate;
+        private DateTimePicker dtpStartDate;
+        private DateTimePicker dtpEndDate;
+        private Button btnSubmit;
+        private Button btnDeny;
+        private Button btnSaveToExcel;
+        private Label lblStatus;
+
+      
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.lblEmployeePosition = new System.Windows.Forms.Label();
+            this.txtEmployeePosition = new System.Windows.Forms.TextBox();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnDeny = new System.Windows.Forms.Button();
+            this.btnSaveToExcel = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            this.lblEmployeeName.AutoSize = true;
+            this.lblEmployeeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeeName.Location = new System.Drawing.Point(43, 36);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(118, 13);
+            this.lblEmployeeName.TabIndex = 0;
+            this.lblEmployeeName.Text = "ПІБ співробітника:";
+
+            this.txtEmployeeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmployeeName.Location = new System.Drawing.Point(213, 36);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(200, 20);
+            this.txtEmployeeName.TabIndex = 1;
+
+            this.lblEmployeePosition.AutoSize = true;
+            this.lblEmployeePosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeePosition.Location = new System.Drawing.Point(43, 76);
+            this.lblEmployeePosition.Name = "lblEmployeePosition";
+            this.lblEmployeePosition.Size = new System.Drawing.Size(141, 13);
+            this.lblEmployeePosition.TabIndex = 2;
+            this.lblEmployeePosition.Text = "Посада співробітника:";
+
+            this.txtEmployeePosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmployeePosition.Location = new System.Drawing.Point(213, 76);
+            this.txtEmployeePosition.Name = "txtEmployeePosition";
+            this.txtEmployeePosition.Size = new System.Drawing.Size(200, 20);
+            this.txtEmployeePosition.TabIndex = 3;
+ 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStartDate.Location = new System.Drawing.Point(43, 116);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(91, 13);
+            this.lblStartDate.TabIndex = 4;
+            this.lblStartDate.Text = "Дата початку:";
+
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndDate.Location = new System.Drawing.Point(43, 156);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(110, 13);
+            this.lblEndDate.TabIndex = 6;
+            this.lblEndDate.Text = "Дата закінчення:";
+
+            this.dtpStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStartDate.Location = new System.Drawing.Point(213, 116);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpStartDate.TabIndex = 5;
+
+            this.dtpEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpEndDate.Location = new System.Drawing.Point(213, 156);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpEndDate.TabIndex = 7;
+ 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(31, 203);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(222, 34);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Відправити у відпустку";
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+
+            this.btnDeny.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeny.Location = new System.Drawing.Point(263, 203);
+            this.btnDeny.Name = "btnDeny";
+            this.btnDeny.Size = new System.Drawing.Size(150, 34);
+            this.btnDeny.TabIndex = 9;
+            this.btnDeny.Text = "Відмовити";
+            this.btnDeny.Click += new System.EventHandler(this.btnDeny_Click);
+
+            this.btnSaveToExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveToExcel.Location = new System.Drawing.Point(31, 243);
+            this.btnSaveToExcel.Name = "btnSaveToExcel";
+            this.btnSaveToExcel.Size = new System.Drawing.Size(382, 30);
+            this.btnSaveToExcel.TabIndex = 10;
+            this.btnSaveToExcel.Text = "Зберегти в Excel";
+            this.btnSaveToExcel.Click += new System.EventHandler(this.btnSaveToExcel_Click);
+
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(213, 280);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 11;
+ 
+            this.ClientSize = new System.Drawing.Size(456, 341);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.lblEmployeePosition);
+            this.Controls.Add(this.txtEmployeePosition);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnDeny);
+            this.Controls.Add(this.btnSaveToExcel);
+            this.Controls.Add(this.lblStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Форма заявки на відпустку";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+      
+
+        
+        }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Windows.Forms;
+
+namespace vacation
+{
+    public partial class Form1 : Form
+    {
+        private string leaveStatus;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string employeeName = txtEmployeeName.Text;
+            string employeePosition = txtEmployeePosition.Text;
+            DateTime startDate = dtpStartDate.Value;
+            DateTime endDate = dtpEndDate.Value;
+
+            if (!string.IsNullOrEmpty(employeeName) && !string.IsNullOrEmpty(employeePosition) && startDate <= endDate)
+            {
+                leaveStatus = "Затверджено";
+                lblStatus.Text = "Заявку на відпустку відправлено!";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                leaveStatus = "Відхилено"; // статус1
+                lblStatus.Text = "Помилка! Перевірте введені дані.";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void btnDeny_Click(object sender, EventArgs e)
+        {
+            leaveStatus = "Відхилено"; // статус2
+            lblStatus.Text = "Відмовлено у відпустці.";
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void btnSaveToExcel_Click(object sender, EventArgs e)
+        {
+            string employeeName = txtEmployeeName.Text;
+            string employeePosition = txtEmployeePosition.Text;
+            DateTime startDate = dtpStartDate.Value;
+            DateTime endDate = dtpEndDate.Value;
+
+            // Ексель
+            string filePath = @"C:\Users\Gera\Desktop\Курсова робота\vacation\Excel\VacationRequests.xlsx";
+
+            XLWorkbook workbook;
+            IXLWorksheet worksheet;
+
+            // перевіка на існування файлу
+            if (System.IO.File.Exists(filePath))
+            {
+                // відкрити
+                workbook = new XLWorkbook(filePath);
+                worksheet = workbook.Worksheet("Заявки на відпустку");
+            }
+            else
+            {
+                // новий файл
+                workbook = new XLWorkbook();
+                worksheet = workbook.Worksheets.Add("Заявки на відпустку");
+                worksheet.Cell(1, 1).Value = "ПІБ співробітника";
+                worksheet.Cell(1, 2).Value = "Посада співробітника";
+                worksheet.Cell(1, 3).Value = "Дата початку";
+                worksheet.Cell(1, 4).Value = "Дата закінчення";
+                worksheet.Cell(1, 5).Value = "Статус";
+            }
+
+            int lastRow = worksheet.LastRowUsed().RowNumber() + 1;
+
+            worksheet.Cell(lastRow, 1).Value = employeeName;
+            worksheet.Cell(lastRow, 2).Value = employeePosition;
+            worksheet.Cell(lastRow, 3).Value = startDate.ToShortDateString();
+            worksheet.Cell(lastRow, 4).Value = endDate.ToShortDateString();
+            worksheet.Cell(lastRow, 5).Value = leaveStatus;
+
+            // Збереження файлу
+            workbook.SaveAs(filePath);
+
+            lblStatus.Text = "Дані збережено в Excel!";
+            lblStatus.ForeColor = System.Drawing.Color.Green;
+        }
+
+    }
+}
